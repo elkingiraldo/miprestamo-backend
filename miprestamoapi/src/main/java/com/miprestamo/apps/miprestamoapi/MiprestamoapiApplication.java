6786fd7805c941f2f4388e72bae97abbec63920d
elkingiraldo91 <egiraldo@egiraldo-TP300LA>
2020-04-18 16:45:05 -0500

    Base project with DB connection

---
+++ miprestamoapi/src/main/java/com/miprestamo/apps/miprestamoapi/MiprestamoapiApplication.java
@@ -0,0 +1,13 @@
+package com.miprestamo.apps.miprestamoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiprestamoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiprestamoapiApplication.class, args);
+	}
+
+}
---
+++ miprestamoapi/src/test/java/com/miprestamo/apps/miprestamoapi/MiprestamoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miprestamo.apps.miprestamoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiprestamoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
